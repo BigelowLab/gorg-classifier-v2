93a6471bbf48848eb19f630ecb625ac8911d3d33
Greg Gavelis <43386190+ggavelis@users.noreply.github.com>
2023-10-05 13:50:03 -0400

    Update prokka_2_table.py
    MAJOR: added proteins and contigs

--- templates/prokka_2_table.py
+++ templates/prokka_2_table.py
@@ -1,105 +1,136 @@
 #!/usr/bin/env python
 
 STR_sample_id = "${sample}"
+PATH_contigs="${contigs}"
+PATH_proteins="${faa}"
 PATH_gff="${gff}"
 STR_classif="${classif}"
 STR_taxid="${taxid}"
 TAB = "\t"
 
-
 import pandas as pd
+import sys
+DF_classif = pd.read_csv(PATH_classif)
 
-with open(PATH_gff) as gff_fh, open(STR_sample_id+"_contigs_proteins_annot.tsv","w") as output:
-	## print header to output tsv
-	print("contig_id","locus_tag","ftype","start","end","strand","prokka_gene","prokka_EC_number","prokka_product","swissprot_gene","swissprot_EC_number","swissprot_product","swissprot_eggNOG","swissprot_KO","swissprot_Pfam","swissprot_CAZy","swissprot_TIGRFAMs",sep=TAB,file=output)
+### I. Read in GTDB-Tk classifications ######  -> DF_classif ________________________________________________________
 
-	for line in gff_fh:
+DF_classif = pd.read_csv(PATH_classif)
+DF_out = DF_classif
 
-		### initialize column values
-		locus_tag=''; Name=''; prokka_gene=''; prokka_EC_number=''; prokka_product=''; swissprot_gene=''; swissprot_EC_number=''; swissprot_product=''; swissprot_eggNOG=''; swissprot_KO=''; swissprot_Pfam=''; swissprot_CAZy='';  swissprot_TIGRFAMs=''
+# Keep only classified SAGS
+DF_classif = DF_classif.loc[DF_classif['classification']!='0']
+DF_classif[['domain','phylum','class','order','family','genus','species']] = DF_classif['classification'].str.split(';', expand=True) # Break ranks into columns
+DF_classif = DF_classif[['sag', 'species',  'genus', 'family', 'order', 'class', 'phylum', 'domain']] # reverse order
 
-		if line.startswith("##FASTA"):
-			break
-		if line.startswith("#"):
-			continue
-		LIST_of_fields = line.strip().split(TAB)
 
-		### Fill required columns
-		#contig_id = LIST_of_fields[0]
-		# change contig_id from "gnl|Prokka|AM-276-A01_1" -> "SCGC_AM-276-A01_contig1"
-		contig_id = LIST_of_fields[0].replace('gnl|Prokka|','')
-		# contig_id = LIST_of_fields[0].replace('gnl|Prokka|','SCGC!').replace('_','_contig').replace('!','_')
-		ftype = LIST_of_fields[2]
-		start = LIST_of_fields[3]
-		end = LIST_of_fields[4]
-		strand = LIST_of_fields[6]
+### II. Read in GTDB lineages, linked to taxIDS ##### --> DF_names _______________________________________________________
+DF_names = pd.read_csv(PATH_lineage_names, sep=tab)
+DF_names[['domain','phylum','class','order','family','genus','species']] = DF_names['lineage'].str.split(';', expand=True) # Break ranks into columns
 
-		## Skip features that are just genes
-		if ftype == "gene": continue
+### III. cross-reference -> DICT_sag2taxid ______________________________________________________
+#(by matching first at the species, then genus if necessary, etc.)
 
-		## Read annots into a dictionary
-		DICT_of_annots = {}
-		LIST_of_annots = LIST_of_fields[-1].split(';')   #annots are always in last field output by prokka
-		for i in LIST_of_annots:
-			split_keyvalue = i.split('=') # key is on right of '=' value is onleft
-			if len(split_keyvalue) != 2 and MODE_verbose == True:
-				print(split_keyvalue)
-			if len(split_keyvalue) == 2:
-				key = split_keyvalue[0]
-				value = split_keyvalue[1]
-				DICT_of_annots[key] = value # load into dictionary
+verbose = False
 
-		## The 'product' annotation may contain additional annots (eggNOG, KO, Pfam, CAZy, & TIGRFAMs) all sep by '^^'
-		if 'product' in DICT_of_annots.keys():
-			STR_product = DICT_of_annots.get('product')
-			if "^^" not in STR_product:
-				prokka_product = STR_product
+### Start writing output file
+#DF_classif = DF_classif.head()
 
-			else:
-				## Load in those additional annotations
-				STR_product = STR_product.replace("%2",",")
-				for LIST_additional_annots in STR_product.split("^^"):
-					items = LIST_additional_annots.split("::")
-					DICT_of_annots[items[0]] = items[1]
+DICT_sag2taxid = {}
 
-				prokka_product = DICT_of_annots.get('product')
+species_found = 0; genera_found = 0; families_found = 0; orders_found = 0; classes_found = 0; phyla_found = 0; domains_found = 0; not_found = 0
 
-		## Fill optional columns
-		if 'locus_tag' in DICT_of_annots.keys(): locus_tag = DICT_of_annots.get('locus_tag')
-		if 'gene' in DICT_of_annots.keys(): prokka_gene = DICT_of_annots.get('gene')
-		if 'eC_number' in DICT_of_annots.keys(): prokka_EC_number = DICT_of_annots.get('eC_number')
-		if 'eggNOG' in DICT_of_annots.keys(): swissprot_eggNOG = DICT_of_annots.get('eggNOG')
-		if 'KO' in DICT_of_annots.keys(): swissprot_KO = DICT_of_annots.get('KO')
-		if 'Pfam' in DICT_of_annots.keys(): swissprot_Pfam = DICT_of_annots.get('Pfam')
-		if 'CAZy' in DICT_of_annots.keys(): swissprot_CAZy = DICT_of_annots.get('CAZy')
-		if 'TIGRFAMs' in DICT_of_annots.keys(): swissprot_TIGRFAMs = DICT_of_annots.get('TIGRFAMs')
+LIST_gtdb_names_not_found_in_namesdmp = []
 
-		print(contig_id,locus_tag,ftype,start,end,strand,prokka_gene,prokka_EC_number,prokka_product,swissprot_gene,swissprot_EC_number,swissprot_product,swissprot_eggNOG,swissprot_KO,swissprot_Pfam,swissprot_CAZy,swissprot_TIGRFAMs,sep=TAB,file=output)
+for index, row in DF_classif.iterrows():
+    DF_this_row = row
+    SAG = DF_this_row['sag']
+    taxid = ''; mapped_to_rank = ''; TAXID_species = ''; TAXID_genus = ''; TAXID_family = ''
 
-# ###### Collect core stats for logging / sample summary
-# DF=pd.read_table("comprehensive_prokka_"+STR_sample_id+".tsv")
-# count_handle = open("prokka_stats_"+STR_sample_id+".csv","w")
+    ### I ### Can we find this SPECIES?
+    rank = "species"
+    STR_species = DF_this_row[rank]
+    if STR_species in DF_names[rank].to_list():
+        DF_name_hits = DF_names.loc[DF_names[rank] == STR_species]
 
-# ## basic counts
-# CDS = len(DF.loc[DF['ftype']=='CDS'])
-# print("CDS",CDS,STR_sample_id,sep=",",file=count_handle)
-# print("rRNA",str(len(DF.loc[DF['ftype']=='rRNA'])),STR_sample_id,sep=",",file=count_handle)
-# print("tRNA",str(len(DF.loc[DF['ftype']=='tRNA'])),STR_sample_id,sep=",",file=count_handle)
+        # print(f'{len(DF_name_hits)} match found')                       # How many species in Names.dmp matched this?
+        TAXID_species = DF_name_hits.iloc[0]['taxid']                   # Take the taxid of first match (there are rare cases [e.g. 3 in a million] of two different taxids having the same 'name_txt')
+        if verbose == True: print("species "+STR_species+" = "+TAXID_species)
+        species_found += 1; taxid= TAXID_species; name=STR_species; mapped_to_rank = rank
+    else:
+        if STR_species != 's__': LIST_gtdb_names_not_found_in_namesdmp.append(STR_species)
+            
+        ### II ### If not, can we find this GENUS?
+        rank = "genus"
+        STR_genus = DF_this_row[rank]
+        if STR_genus in DF_names[rank].to_list():
+            DF_name_hits = DF_names.loc[DF_names[rank] == STR_genus]
+            TAXID_genus = DF_name_hits.iloc[0]['taxid']
+            if verbose == True: print("genus "+STR_genus+" = "+TAXID_genus)
+            genera_found += 1; taxid= TAXID_genus; name=STR_genus; mapped_to_rank = rank
+        else:
+            if STR_genus != 'g__': LIST_gtdb_names_not_found_in_namesdmp.append(STR_genus)
+                
+            ### III ### If not, can we find the FAMILY?
+            rank = "family"
+            STR_family = DF_this_row[rank]
+            if STR_family in DF_names[rank].to_list():
+                DF_name_hits = DF_names.loc[DF_names[rank] == STR_family]
+                TAXID_family = DF_name_hits.iloc[0]['taxid']
+                if verbose: print("family "+STR_family+" = "+TAXID_family)
+                families_found += 1; taxid = TAXID_family; name=STR_family; mapped_to_rank = rank
+            else:
+                if STR_family != 'f__': LIST_gtdb_names_not_found_in_namesdmp.append(STR_family)
+                    
+                ### IV ### If not, can we find the ORDER?
+                rank = "order"
+                STR_order = DF_this_row[rank]
+                if STR_order in DF_names[rank].to_list():
+                    DF_name_hits = DF_names.loc[DF_names[rank] == STR_order]
+                    TAXID_order = DF_name_hits.iloc[0]['taxid']
+                    if verbose: print("order "+STR_order+" = "+TAXID_order)
+                    orders_found +=1; taxid = TAXID_order; name=STR_order; mapped_to_rank = rank     
+                else:
+                    if STR_order != 'o__': LIST_gtdb_names_not_found_in_namesdmp.append(STR_order)
+                        
+                    ### V ### If not, can we find the CLASS?
+                    rank = "class"
+                    STR_class = DF_this_row[rank]
+                    if STR_class in DF_names[rank].to_list():
+                        DF_name_hits = DF_names.loc[DF_names[rank] == STR_class]
+                        TAXID_class = DF_name_hits.iloc[0]['taxid']
+                        if verbose: print("class " + STR_class + " " + TAXID_class)
+                        classes_found += 1; taxid = TAXID_class; name=STR_class; mapped_to_rank = rank    
+                    else:
+                        if STR_class != 'c__': LIST_gtdb_names_not_found_in_namesdmp.append(STR_class)
+                            
+                        ### VI ### If not, can we find the PHYLUM?
+                        rank = "phylum"
+                        STR_phylum = DF_this_row[rank]
+                        if STR_phylum in DF_names[rank].to_list():
+                            DF_name_hits = DF_names.loc[DF_names[rank] == STR_phylum]
+                            TAXID_phylum = DF_name_hits.iloc[0]['taxid']
+                            if verbose: print("order "+STR_phylum+" = "+TAXID_phylum)
+                            phyla_found += 1; taxid = TAXID_phylum; name=STR_phylum; mapped_to_rank = rank         
+                        else:
+                            if STR_phylum != 'p__': LIST_gtdb_names_not_found_in_namesdmp.append(STR_phylum)
+                            print("SAG "+SAG+" not placed to phylum")
+                            not_found += 1
+                            
+    DICT_sag2taxid[SAG] = str(taxid)
+                            
+##### Summarize
+print('species found: '+str(species_found))
+print('genera found: '+str(genera_found))
+print('families found: '+str(families_found))
+print('orders found: '+str(orders_found))
+print('classes found: '+str(classes_found))
+print('phyla found: '+str(phyla_found))
 
-# ## calc percent_CDS_annotated and average_CDS_length
-# DF_CDS=DF.loc[DF['ftype']=='CDS']
-# if len(DF_CDS) > 0:
-# 	if 'hypothetical protein' not in DF_CDS['prokka_product'].tolist():
-# 		percent_CDS_annotated = 100
-# 	else:
-# 		NUM_annotated = CDS - DF_CDS['prokka_product'].value_counts()['hypothetical protein']
-# 		percent_CDS_annotated = round(NUM_annotated / CDS * 100, 2)
-# 	print("percent_CDS_annotated",str(percent_CDS_annotated),STR_sample_id,sep=",",file=count_handle)
+if len(LIST_gtdb_names_not_found_in_namesdmp) > 0:
+    sys.exit("gtdb taxon names that were not found in "+PATH_lineage_names+": "+",".join(LIST_gtdb_names_not_found_in_names))
+    
+#### IV. ####### Save output ###### ______________________________________________________________________________________\
 
-# 	DF_CDS['length']=DF_CDS['end']-DF_CDS['start']
-# 	average_CDS_length=round(DF_CDS['length'].mean(), 2)
-# 	print("average_CDS_length",str(average_CDS_length),STR_sample_id,sep=",",file=count_handle)
-
-# print(DF_CDS['prokka_product']) ###gremlin
-# count_handle.close()
-
+SER_taxids = DF_out['sag'].map(DICT_sag2taxid)
+DF_out['taxid'] = SER_taxids ### Adds in a new 'taxid' column with the findings
+DF_out.to_csv(PATH_out)
